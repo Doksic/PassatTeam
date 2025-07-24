df90e270fc78a7f3be5e418ca2d0b9757fa1703b
Doksic <135099596+Doksic@users.noreply.github.com>
2025-07-24 23:04:19 +0300

    Start PassatTeam

---
+++ src/main/java/com/Passat/Passat/PassatApplication.java
@@ -0,0 +1,13 @@
+package com.Passat.Passat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassatApplication.class, args);
+	}
+
+}
